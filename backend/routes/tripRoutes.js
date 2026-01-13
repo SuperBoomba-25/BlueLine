db660f8cb9a830310cce2423c6b397b4026ab902
Alon Sasson <alonsasson497@gmail.com>
2026-01-13 17:41:46 +0200

    Use bcrypt instead of bcryptjs to fix Render deploy

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Trip = require("../models/Trip");
+const Trip = require("../models/Trips");
 
 const { protect } = require("../middleware/authMiddleware");
 
